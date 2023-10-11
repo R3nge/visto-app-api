87b53cef638b7f74e792860725d51425be35fd1c
Digo <digo.rodrigomalaquias@hotmail.com>
2023-10-11 12:37:21 -0300

    subindo

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from "express";
-import routes from "./routes";
 import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 
 mongoose
   .connect(
